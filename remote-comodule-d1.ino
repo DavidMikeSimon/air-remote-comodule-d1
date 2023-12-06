30e50c2c2e0e7e3094b65b0f16db22e900a4f1d5
David Simon <david.simon@color.com>
2023-12-05 21:19:47 -0500

    more stuff

--- remote-comodule-d1.ino
+++ remote-comodule-d1.ino
@@ -5,6 +5,16 @@
 
 #include "secrets.h"
 
+/*
+ * Event types:
+ * A - ASCII key
+ * C - Consumer control scan code
+ * K - Keyboard scan code (for non-ASCII keys)
+ * N - Network connected
+ * O - OK button
+ * W - Power button
+ */
+
 #define IR_RECEIVE_PIN D0
 #define TX_PIN D4
 #define RX_PIN D7
@@ -12,89 +22,85 @@
 WiFiClient net;
 MQTTClient client;
 
-void connectMqtt() {
-  while (!client.connect("remote-comodule-d1", MQTT_USER, MQTT_PASS)) {
-    delay(500);
-  }
-  client.publish("/air-remote-debug", "CONNECTED");
+bool currentPassthruFlag = true;
+bool intendedPassthruFlag = true;
+unsigned long lastPwrSignal = 0;
+char outputBuf[64];
+
+void writePassthru(bool value) {
+  Wire.beginTransmission(0x05);
+  Wire.write(value ? 'P' : 'p');
+  Wire.endTransmission();
+  currentPassthruFlag = value;
 }
 
-void setup() {
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-	Serial.begin(19200);
-  Wire.begin();
-
-  pinMode(IR_RECEIVE_PIN, INPUT_PULLUP);
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+void connectMqtt() {
+  // If we lose network connection, fall back to passthru mode
+  intendedPassthruFlag = true;
+  writePassthru(true);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
 
-  client.begin(MQTT_HOST, net);
-  connectMqtt();
-  client.publish("/air-remote-debug", "SETUP DONE");
+  while (!client.connect("remote-comodule-d1", MQTT_USER, MQTT_PASS)) {
+    delay(500);
+  }
+
+  client.subscribe("/air-remote/passthru-setting");
+  client.publish("/air-remote/events", "{ \"event\": \"N\" }");
 }
 
-unsigned long lastPwrSignal = 0;
+void messageReceived(String &topic, String &payload) {
+  if (topic == "/air-remote/passthru-setting") {
+    intendedPassthruFlag = payload == "ON";
+  }
+}
 
-char buf[128];
+void setup() {
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-void loop() { 
+	//Serial.begin(19200);
+  Wire.begin();
+
+  pinMode(IR_RECEIVE_PIN, INPUT_PULLUP);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+
+  client.begin(MQTT_HOST, net);
+  client.onMessage(messageReceived);
+}
+
+void loop() {
+  client.loop();
+  delay(10);
+
+  if (!client.connected()) {
+    connectMqtt();
+  }
+
+  if (currentPassthruFlag != intendedPassthruFlag) {
+    writePassthru(intendedPassthruFlag);
+  }
+
   if (IrReceiver.decode()) {
     if (IrReceiver.decodedIRData.decodedRawData == 0x854) {
-      if (millis() - lastPwrSignal > 500) {
-        if (client.connected()) {
-          client.publish("/air-remote-debug", "PWR BTN");
-        }
+      if (millis() - lastPwrSignal > 500 && client.connected()) {
+        client.publish("/air-remote/events", "{ \"event\": \"W\" }");
       }
       lastPwrSignal = millis();
     }
     IrReceiver.resume();
   }
 
-  // if (Serial.available() > 0) {
-  //   auto bytesAvail = Serial.available();
-  //   //auto bytesRead = Serial.readBytesUntil('\n', serialBuf, sizeof(serialBuf));
-  //   auto bytesRead = Serial.readBytes(serialBuf, sizeof(serialBuf));
-  //   if (bytesRead > 0 && serialBuf[0] == 'H') {
-  //     client.publish("/air-remote-debug", serialBuf, bytesRead);
-  //   } else {
-  //     char buffer[40];
-  //     sprintf(buffer, "RX FAIL, AVAIL %u, READ %u, FIRST CHAR %u", bytesAvail, bytesRead, serialBuf[0]);
-  //     client.publish("/air-remote-debug", buffer);
-  //   }
-  // }
-
-  Wire.beginTransmission(1);
-  Wire.write("PING", 4);
-  Wire.endTransmission();
-
-  Wire.requestFrom(0x05, 4);
-  unsigned int lenRead = 0;
-  while (Wire.available()) {
-    if (lenRead >= 128) {
-      lenRead = 0;
-      Serial.println("OVERFLOW");
-      client.publish("/air-remote-debug", "Buffer overflow");
-      break;
+  Wire.requestFrom(0x05, 2);
+  if (Wire.available()) {
+    uint8_t kind = Wire.read();
+    if (Wire.available()) {
+      uint8_t data = Wire.read();
+      if (kind >= 'A' && kind <= 'Z' && client.connected()) {
+        int len = sprintf(outputBuf, "{ \"event\": \"%c\", \"data\": \"0x%02X\" }", kind, data);
+        client.publish("/air-remote/events", outputBuf, len);
+      }
     }
-    char c = Wire.read();
-    buf[lenRead] = c;
-    lenRead++;
   }
-  if (lenRead > 0) {
-    buf[lenRead+1] = NULL;
-    Serial.println(buf);
-    client.publish("/air-remote-debug", buf, lenRead);
-  }
-
-  client.loop();
-  if (!client.connected()) {
-    Serial.println("NOT ONLINE?");
-    connectMqtt();
-  }
-
-  delay(1000);
 }
